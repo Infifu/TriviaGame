ecdda7246115dc28fccb7cccba54beadda3cd070
unknown <shmuellazo@gmail.com>
2025-04-18 17:23:16 +0300

    1.0.2 finish :)

--- Trivia_Shmuel_Ilya_2025/LoginRequestHandler.h
+++ Trivia_Shmuel_Ilya_2025/LoginRequestHandler.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "IRequestHandler.h"
 
-class LoginRequestHandler : public IRequestHandler {
-
+class LoginRequestHandler : public IRequestHandler 
+{
+public:
+    bool isRequestRelevant(const RequestInfo& requestInfo) override;
+    RequestResult handleRequest(const RequestInfo& requestInfo) override;
 };
