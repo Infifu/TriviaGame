322a60e4ee9d3253348c6f5a41a6f2ef304cbe48
Infifu <iliiniliya2008@gmail.com>
2025-04-19 19:41:18 +0300

    added RequestHandlerFactory and implemented

--- Trivia_Shmuel_Ilya_2025/IDatabase.h
+++ Trivia_Shmuel_Ilya_2025/IDatabase.h
@@ -8,7 +8,10 @@
 
 class IDatabase
 {
-public: 
+public:
+	virtual bool open() = 0;
+	virtual bool close() = 0;
+
 	virtual bool doesUserExist(std::string username) = 0;
 	virtual bool doesPasswordMatch(std::string username, std::string password) = 0;
 	virtual void addNewUser(std::string username, std::string password, std::string email) = 0;
--- Trivia_Shmuel_Ilya_2025/SqliteDataBase.h
+++ Trivia_Shmuel_Ilya_2025/SqliteDataBase.h
@@ -6,11 +6,18 @@ typedef std::vector<std::map<std::string, std::string>> dbVector;
 
 class SqliteDataBase : public IDatabase
 {
+public:
+	bool open() override;
+	bool close() override;
+
 	bool doesUserExist(std::string username) override;
 	bool doesPasswordMatch(std::string username, std::string password) override;
 	void addNewUser(std::string username, std::string password, std::string email) override;
 
 	dbVector selectQuery(std::string column, std::string argument);
 	bool insertQuery(std::map<std::string, std::string> values);
+
+private:
+	sqlite3* _database;
 };
 
