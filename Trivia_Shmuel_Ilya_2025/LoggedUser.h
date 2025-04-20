8b368a2a2222ffb1a177d1ea47774388fb1c720a
Infifu <iliiniliya2008@gmail.com>
2025-04-20 18:55:34 +0300

    Fully working Login and signup

--- Trivia_Shmuel_Ilya_2025/LoggedUser.h
+++ Trivia_Shmuel_Ilya_2025/LoggedUser.h
@@ -8,6 +8,10 @@ public:
     LoggedUser(const std::string& username) : m_username(username) {}
     std::string getUsername() const { return m_username; }
 
+    //TO DO:
+    //need this for log out might delete later sir
+    bool operator==(const LoggedUser& other) const { return getUsername() == other.getUsername(); }
 private:
+
     std::string m_username;
 };
--- Trivia_Shmuel_Ilya_2025/LoginRequestHandler.h
+++ Trivia_Shmuel_Ilya_2025/LoginRequestHandler.h
@@ -1,9 +1,16 @@
 #pragma once
 #include "IRequestHandler.h"
+#include "LoggedUser.h"
+#include "SqliteDataBase.h"
 
 class LoginRequestHandler : public IRequestHandler 
 {
 public:
     bool isRequestRelevant(const RequestInfo& requestInfo) override;
     RequestResult handleRequest(const RequestInfo& requestInfo) override;
+private:
+    std::vector<LoggedUser> _loggedUsers;
+    RequestResult login(RequestInfo);
+    RequestResult signup(RequestInfo);
+    void logOut(LoggedUser user);
 };
