d7eb73dad0a50046ce3f5bccdda8ec44183c8952
Infifu <iliiniliya2008@gmail.com>
2025-05-17 22:16:08 +0300

    Fixed join screen rooms, added buttons implementation in most cases

--- TriviaClient/Views/JoinRoomView.xaml.cs
+++ TriviaClient/Views/JoinRoomView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TriviaClient.ViewModel;
 
 namespace TriviaClient.Views
 {
@@ -19,10 +21,23 @@ namespace TriviaClient.Views
     /// </summary>
     public partial class JoinRoomView : Window
     {
+        public ObservableCollection<Room> Rooms { get; set; }
         public JoinRoomView()
         {
             InitializeComponent();
+
+            Rooms = new ObservableCollection<Room>
+            {
+                new Room("SagiRoom", 6,30),
+                new Room("ShmuelRoom", 4,20),
+                new Room("OriRoom", 4,10)
+            };
+
+            // Set DataContext for data binding
+            DataContext = this;
         }
+
+      
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
@@ -39,6 +54,29 @@ namespace TriviaClient.Views
         private void btnClose_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void JoinRoom_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            Room selectedRoom = btn.Tag as Room;
+
+            if (selectedRoom != null)
+            {
+                RoomPlayerInfoPanell.Visibility = Visibility.Visible;
+                RoomNameShow.Text = "Room name: " + selectedRoom.Name;
+                PlayerAmountShow.Text = "Player amount: " + selectedRoom.PlayerCount.ToString();
+                TimeToAnswerShow.Text = "Time to answer (seconds): " + selectedRoom.TimeToAnswer.ToString();
+            }
+
+
+        }
+
+        private void BackToMenu_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.Show();
+            this.Hide();
         }
     }
 }
