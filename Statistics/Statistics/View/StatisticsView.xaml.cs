199acff2204a008414ee3acce2f4b5b57ffa1e9c
shmuel Ito <shmuellazo@gmail.com>
2025-05-17 19:33:54 +0300

    add Statistics Screen

---
+++ Statistics/Statistics/View/StatisticsView.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Statistics.View
+{
+    /// <summary>
+    /// Логика взаимодействия для StatisticsView.xaml
+    /// </summary>
+    public partial class StatisticsView : Window
+    {
+        public StatisticsView()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
