4a6ae08cb112f92c6fbae3f7e79c9795b2268978
Infifu <iliiniliya2008@gmail.com>
2025-05-17 16:37:35 +0300

    Login / Register screen added

---
+++ TriviaClient/Views/LoginRegisterScreen.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFTEST.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginRegisterScreen.xaml
+    /// </summary>
+    public partial class LoginRegisterScreen : Window
+    {
+        public LoginRegisterScreen()
+        {
+            InitializeComponent();
+        }
+
+
+        //return the ability to drag the window after removing titlebar
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton==MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void btnSwitchToRegister_Click(object sender, RoutedEventArgs e)
+        {
+            LoginPanel.Visibility = Visibility.Collapsed;
+            RegisterPanel.Visibility = Visibility.Visible;
+        }
+        private void btnSwitchToLogin_Click(object sender, RoutedEventArgs e)
+        {
+            LoginPanel.Visibility = Visibility.Visible;
+            RegisterPanel.Visibility = Visibility.Collapsed;
+        }
+    }
+}
