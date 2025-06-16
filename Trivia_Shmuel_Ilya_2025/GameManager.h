dff98d02884afcba0a480724665f80da943f64ae
Infifu <iliiniliya2008@gmail.com>
2025-06-16 21:19:42 +0300

    GameManager added

---
+++ Trivia_Shmuel_Ilya_2025/GameManager.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Game.h"
+#include "Room.h"
+
+
+class GameManager
+{
+private:
+	IDatabase* m_database;
+	std::vector<Game> m_games;
+public:
+	Game createGame(Room room);
+	void deleteGame(unsigned int gameId);
+};
+
