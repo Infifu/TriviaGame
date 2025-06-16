5a3891cfe342e3f18cc39647bf34107be71f17d7
Infifu <iliiniliya2008@gmail.com>
2025-06-16 21:13:29 +0300

    Game Manager again

--- Trivia_Shmuel_Ilya_2025/LoggedUser.h
+++ Trivia_Shmuel_Ilya_2025/LoggedUser.h
@@ -12,7 +12,10 @@ public:
     //need this for log out might delete later sir
     bool operator==(const LoggedUser& other) const { return getUsername() == other.getUsername(); }
     bool operator<(const LoggedUser& other) const { return getUsername() < other.getUsername(); }
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 private:
 
     std::string m_username;
